eb223eb0a22c951f44c290ae414c7347554ec505
vundi <vundi4773@gmail.com>
2024-06-15 00:18:36 +0300

    use Effect

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      const timerId = setTimeout(() => {
+        setTimeRemaining(timeRemaining - 1);
+      }, 1000);
+
+      // Cleanup function to clear the timeout
+      return () => clearTimeout(timerId);
+    } else {
+      setTimeRemaining(10);     
+      onAnswered(false); 
+    }
+  }, [timeRemaining, onAnswered]); 
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
